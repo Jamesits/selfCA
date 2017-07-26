b307a9cb5dfd90b0c2e2edb3df951b38cfc170e7
James Swineson <i@swineson.me>
2017-07-26 16:45:54 +0800

    fix typo

--- 03-create-intermediate.sh
+++ 03-create-intermediate.sh
@@ -16,11 +16,11 @@ chmod 400 "$INTERMEDIATE_CERT_NAME/private/$INTERMEDIATE_CERT_NAME.key.pem"
 
 # Generate CSR
 echo "You will be prompted to enter your intermediate key pass phrase again, and information for your intermediate certificate. "
-openssl req -config "$INTERMEDIATE_CERT_NAME/openssl.cnf" -new -sha256 -extentions san_env -key "$INTERMEDIATE_CERT_NAME/private/$INTERMEDIATE_CERT_NAME.key.pem" -out "$INTERMEDIATE_CERT_NAME/csr/$INTERMEDIATE_CERT_NAME.csr.pem" || die "Unable to generate CSR, maybe wrong intermediate key passphrase?"
+openssl req -config "$INTERMEDIATE_CERT_NAME/openssl.cnf" -new -sha256 -extensions san_env -key "$INTERMEDIATE_CERT_NAME/private/$INTERMEDIATE_CERT_NAME.key.pem" -out "$INTERMEDIATE_CERT_NAME/csr/$INTERMEDIATE_CERT_NAME.csr.pem" || die "Unable to generate CSR, maybe wrong intermediate key passphrase?"
 
 # Sign with root cert
 echo "You will be prompted to enter your root key pass phrase"
-openssl ca -config openssl.cnf -extensions v3_intermediate_ca -days $INTERMEDIATE_CERT_DAYS -notext -md sha256 -extentions san_env -in "$INTERMEDIATE_CERT_NAME/csr/$INTERMEDIATE_CERT_NAME.csr.pem" -out "$INTERMEDIATE_CERT_NAME/certs/$INTERMEDIATE_CERT_NAME.cert.pem" || die "Unable to sign cert, maybe wrong root key pass phrase?"
+openssl ca -config openssl.cnf -extensions v3_intermediate_ca -days $INTERMEDIATE_CERT_DAYS -notext -md sha256 -extensions san_env -in "$INTERMEDIATE_CERT_NAME/csr/$INTERMEDIATE_CERT_NAME.csr.pem" -out "$INTERMEDIATE_CERT_NAME/certs/$INTERMEDIATE_CERT_NAME.cert.pem" || die "Unable to sign cert, maybe wrong root key pass phrase?"
 
 chmod 444 "$INTERMEDIATE_CERT_NAME/certs/$INTERMEDIATE_CERT_NAME.cert.pem"
 
